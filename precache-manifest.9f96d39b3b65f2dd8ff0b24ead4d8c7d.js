192be38845c1afdae0adc30c4be255b8a896f249
babs105 <mamembaye105@live.fr>
2020-01-15 10:33:00 +0000

    Updates

---
+++ precache-manifest.9f96d39b3b65f2dd8ff0b24ead4d8c7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4b84e4acede37722f1465dca0962b6",
+    "url": "/appcarbufe/index.html"
+  },
+  {
+    "revision": "80b846e4bab0ba34c64e",
+    "url": "/appcarbufe/static/css/main.02ba8d78.chunk.css"
+  },
+  {
+    "revision": "2c64b4166d5b59e93516",
+    "url": "/appcarbufe/static/js/2.d21e84f9.chunk.js"
+  },
+  {
+    "revision": "5a0d989568a936270ef6c2e0e2d0229c",
+    "url": "/appcarbufe/static/js/2.d21e84f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80b846e4bab0ba34c64e",
+    "url": "/appcarbufe/static/js/main.58e4d560.chunk.js"
+  },
+  {
+    "revision": "2936daf9d4e241edea0e",
+    "url": "/appcarbufe/static/js/runtime-main.6f3dfe05.js"
+  }
+]);
